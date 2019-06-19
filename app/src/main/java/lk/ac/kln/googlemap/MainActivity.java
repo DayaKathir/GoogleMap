6332516a1c386042d25448f2b6214cf4aa9d46ce
fct <vijithasenadeera53@gmail.com>
2019-06-19 15:35:28 +0530

    google map api

---
+++ app/src/main/java/lk/ac/kln/googlemap/MainActivity.java
@@ -0,0 +1,35 @@
+package lk.ac.kln.googlemap;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+        LatLng fct = new LatLng(6.957137, 79.888579);
+        MarkerOptions options = new MarkerOptions().position(fct).title("Faculty of Computing and Technology");
+        map.addMarker(options);
+        map.moveCamera(CameraUpdateFactory.newLatLng(fct));
+    }
+
+}
